2ccf59881477e8cda41daded5307e9bc19ad6acc
Mr Heap Allocator <58353903+MrNullPointer@users.noreply.github.com>
2021-02-21 14:55:54 -0500

    Starter Code 0.0

---
+++ build/CMakeFiles/monitor.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mrheapallocator/Documents/Documents/ubuntu/Projects/LinuxActivityMonitor/src/format.cpp" "/home/mrheapallocator/Documents/Documents/ubuntu/Projects/LinuxActivityMonitor/build/CMakeFiles/monitor.dir/src/format.cpp.o"
+  "/home/mrheapallocator/Documents/Documents/ubuntu/Projects/LinuxActivityMonitor/src/linux_parser.cpp" "/home/mrheapallocator/Documents/Documents/ubuntu/Projects/LinuxActivityMonitor/build/CMakeFiles/monitor.dir/src/linux_parser.cpp.o"
+  "/home/mrheapallocator/Documents/Documents/ubuntu/Projects/LinuxActivityMonitor/src/main.cpp" "/home/mrheapallocator/Documents/Documents/ubuntu/Projects/LinuxActivityMonitor/build/CMakeFiles/monitor.dir/src/main.cpp.o"
+  "/home/mrheapallocator/Documents/Documents/ubuntu/Projects/LinuxActivityMonitor/src/ncurses_display.cpp" "/home/mrheapallocator/Documents/Documents/ubuntu/Projects/LinuxActivityMonitor/build/CMakeFiles/monitor.dir/src/ncurses_display.cpp.o"
+  "/home/mrheapallocator/Documents/Documents/ubuntu/Projects/LinuxActivityMonitor/src/process.cpp" "/home/mrheapallocator/Documents/Documents/ubuntu/Projects/LinuxActivityMonitor/build/CMakeFiles/monitor.dir/src/process.cpp.o"
+  "/home/mrheapallocator/Documents/Documents/ubuntu/Projects/LinuxActivityMonitor/src/processor.cpp" "/home/mrheapallocator/Documents/Documents/ubuntu/Projects/LinuxActivityMonitor/build/CMakeFiles/monitor.dir/src/processor.cpp.o"
+  "/home/mrheapallocator/Documents/Documents/ubuntu/Projects/LinuxActivityMonitor/src/system.cpp" "/home/mrheapallocator/Documents/Documents/ubuntu/Projects/LinuxActivityMonitor/build/CMakeFiles/monitor.dir/src/system.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/processor.cpp
@@ -0,0 +1,5 @@
+
+#include "processor.h"
+
+// TODO: Return the aggregate CPU utilization
+float Processor::Utilization() { return 0.0; }
